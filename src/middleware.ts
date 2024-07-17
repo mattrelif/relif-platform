0cf7860cb767b245c37583e3cf05d704745387ad
Anthony Vinicius <anthony.vii27@gmail.com>
2024-07-17 09:17:19 -0300

    fix: internacionalization middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,9 +1,8 @@
+import { i18n } from "@/app/i18n-config";
 import { match as matchLocale } from "@formatjs/intl-localematcher";
 import Negotiator from "negotiator";
 import type { NextRequest } from "next/server";
 import { NextResponse } from "next/server";
-
-import { i18n } from "./i18n-config";
 
 function getLocale(request: NextRequest): string | undefined {
     const negotiatorHeaders: Record<string, string> = {};
