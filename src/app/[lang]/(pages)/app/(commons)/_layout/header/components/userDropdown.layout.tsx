ce79cad45740227e75917d2e2e2578c2bd413d62
Anthony Vinicius <anthony.vii27@gmail.com>
2024-07-20 20:09:12 -0300

    feat: links on user dropdown

--- src/app/[lang]/(pages)/app/(commons)/_layout/header/components/userDropdown.layout.tsx
+++ src/app/[lang]/(pages)/app/(commons)/_layout/header/components/userDropdown.layout.tsx
@@ -12,9 +12,13 @@ import {
     DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import { ReactNode } from "react";
 
 const UserDropdown = ({ children }: { children: ReactNode }): ReactNode => {
+    const pathname = usePathname();
+    const urlPath = pathname.split("/").slice(0, 4).join("/");
+
     return (
         <DropdownMenu>
             <DropdownMenuTrigger>{children}</DropdownMenuTrigger>
@@ -22,25 +26,31 @@ const UserDropdown = ({ children }: { children: ReactNode }): ReactNode => {
                 <DropdownMenuLabel>Hi, Anthony</DropdownMenuLabel>
                 <DropdownMenuSeparator />
                 <DropdownMenuItem asChild>
-                    <Link href="#">Profile</Link>
+                    <Link href={`${urlPath}/preferences/my-profile`}>Profile</Link>
                 </DropdownMenuItem>
                 <DropdownMenuSub>
                     <DropdownMenuSubTrigger>Preferences</DropdownMenuSubTrigger>
                     <DropdownMenuSubContent>
                         <DropdownMenuItem>
-                            <Link href="#">Team</Link>
+                            <Link href={`${urlPath}/preferences/my-organization/users`}>Team</Link>
                         </DropdownMenuItem>
                         <DropdownMenuItem>
-                            <Link href="#">Organization</Link>
+                            <Link href={`${urlPath}/preferences/my-organization/overview`}>
+                                Organization
+                            </Link>
                         </DropdownMenuItem>
                         <DropdownMenuItem>
-                            <Link href="#">Platform</Link>
+                            <Link href={`${urlPath}/preferences/platform`}>Platform</Link>
                         </DropdownMenuItem>
                         <DropdownMenuItem>
-                            <Link href="#">Support</Link>
+                            <Link href={`${urlPath}/preferences/support`}>Support</Link>
                         </DropdownMenuItem>
                     </DropdownMenuSubContent>
                 </DropdownMenuSub>
+                <DropdownMenuSeparator />
+                <DropdownMenuItem asChild>
+                    <Link href="#">Logout</Link>
+                </DropdownMenuItem>
             </DropdownMenuContent>
         </DropdownMenu>
     );
