698a14897e71f3348bb487859293cc78843e5940
Anthony Vinicius <anthony.vii27@gmail.com>
2024-08-29 17:41:33 -0300

    fix: host

--- src/lib/axios-client.ts
+++ src/lib/axios-client.ts
@@ -2,7 +2,7 @@ import { BFFConfig } from "@/config/bff";
 import axios, { AxiosInstance } from "axios";
 
 const client: AxiosInstance = axios.create({
-    baseURL: BFFConfig.localhost,
+    baseURL: BFFConfig.host,
     headers: {
         "Content-Type": "application/json",
         "Cache-Control": "no-cache",
